4d91ebdd4c7f8bede18511f3ea6c1ffa7ee32a66
Reginald96 <reginaldjoshua.young@my.jcu.edu.au>
2020-01-26 10:53:56 +0800

    removed page sidebar

--- public/wp-content/themes/testingTheme/page.php
+++ public/wp-content/themes/testingTheme/page.php
@@ -5,22 +5,6 @@
 <div class="container">
 
 
-		<section class="row">
-
-			<div class="col-lg-3">
-
-						<?php if(is_active_sidebar('page-sidebar')):?>
-
-								<?php dynamic_sidebar('page-sidebar');?>
-
-						<?php endif;?>
-
-
-			</div>
-
-
-
-			<div class="col-lg-9">
 
 
 				<h1><?php the_title();?></h1>
@@ -33,10 +17,6 @@
 
 				<?php get_template_part('includes/section','content');?>
 
-
-			</div>
-
-		</section>
 
 
 
--- public/wp-content/themes/testingTheme/searchform.php
+++ public/wp-content/themes/testingTheme/searchform.php
@@ -1,6 +1,6 @@
 <form action="/" method="get">
 
-		<label for="search">Search: </label>
+		<label for="search"></label>
 		<input type="text" name="s" id="search" value="<?php the_search_query();?>" required>
 
 		<button type="submit">Go!</button>
